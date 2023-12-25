d5e20c15caf9f306a6fb77e9e58788e275b203d1
Anar Safarov <anarsferov@gmail.com>
2023-12-25 17:33:20 +0400

    fix: contact validation

--- src/api/contacts/contacts.valitate.ts
+++ src/api/contacts/contacts.valitate.ts
@@ -3,19 +3,26 @@ import * as z from 'zod';
 export const ValidateContact = z.object({
     contact: z
         .string()
+        .trim()
+        .min(1, { message: 'provide username or email' })
         .refine(
             (value) => {
                 const isEmail = /\S+@\S+\.\S+/.test(value);
                 const isNonEmptyString = typeof value === 'string' && value.trim().length > 0;
+                if (value.includes('@')) {
+                    return isEmail;
+                }
 
-                return isEmail || isNonEmptyString;
+                return isNonEmptyString;
             },
             {
-                message: 'Contact must be either a valid email or a valid provider id'
+                message: 'Please enter a valid email or username'
             }
         )
         .refine((contact) => contact.toLowerCase())
 });
+
+export type ValidateContactType = z.infer<typeof ValidateContact>;
 
 export type ValidateContact = z.infer<typeof ValidateContact>;
 
